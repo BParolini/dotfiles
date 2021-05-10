91b478c2636eb609ba0b211ef35b08a5cf1cd7d0
Bruno Parolini <brunoparolini@gmail.com>
2021-05-09 23:40:46 -0300

    Added Steam env variables configuration

--- scripts/home/steam_env.sh
+++ scripts/home/steam_env.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs Steam
+# description: Prepares Steam environment
 
-export LD_PRELOAD=/usr/lib64/libfreetype.so.6
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
+
+STEAM_APP_PATH=$(which steam)
+
+alias steam="env LD_PRELOAD=/usr/lib64/libfreetype.so.6 $STEAM_APP_PATH"
