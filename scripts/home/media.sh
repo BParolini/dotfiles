7f5ffd770e236345f0db923b71d44777f7baa23d
Bruno Parolini <brunoparolini@gmail.com>
2021-05-14 10:32:20 -0300

    Simple script adjustment

--- scripts/home/media.sh
+++ scripts/home/media.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs all gstreamer and pulseaudio packages and some media players
+# description: Installs all pipewire and gstreamer packages and some media players
 
 dnf install -y $(dnf search pipewire | grep ^pipewire | grep -v devel | grep -v doc | grep -v jack | awk '{print $1}' | uniq | sort) \
                $(dnf search gstreamer | grep ^gstreamer1*- | grep -v svt | grep -v devel | grep -v i686 | awk '{print $1}' | sort | uniq) \
