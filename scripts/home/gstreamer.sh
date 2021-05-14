3d83612de5b025756ca661f3dc81fc3df68ee38d
Bruno Parolini <brunoparolini@gmail.com>
2021-05-14 10:31:49 -0300

    Added pipewire and removed pulseaudio (Fedora 34)

--- scripts/home/gstreamer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs all gstreamer and pulseaudio packages and some media players
-
-dnf install -y $(dnf search gstreamer | grep ^gstreamer1*- | grep -v svt | grep -v devel | grep -v i686 | awk '{print $1}' | sort | uniq) \
-                vlc vlc-core vlc-extras \
-                clementine
---
+++ scripts/home/media.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# author: Bruno Parolini brunoparolini@gmail.com
+# description: Installs all gstreamer and pulseaudio packages and some media players
+
+dnf install -y $(dnf search pipewire | grep ^pipewire | grep -v devel | grep -v doc | grep -v jack | awk '{print $1}' | uniq | sort) \
+               $(dnf search gstreamer | grep ^gstreamer1*- | grep -v svt | grep -v devel | grep -v i686 | awk '{print $1}' | sort | uniq) \
+               vlc vlc-core vlc-extras \
+               clementine
