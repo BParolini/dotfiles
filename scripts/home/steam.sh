288e8243a08ffd3a3fc684ee5af9e6a8333662f9
Bruno Parolini <brunoparolini@gmail.com>
2021-05-14 15:27:02 -0300

    Adjusted steam script and removed steam_env

--- scripts/home/steam.sh
+++ scripts/home/steam.sh
@@ -5,5 +5,8 @@
 
 dnf install -y steam
 
-# STEAM_APP_PATH=$(which steam)
-# alias steam="env QT_AUTO_SCREEN_SCALE_FACTOR=0 LD_PRELOAD=/usr/lib64/libfreetype.so.6 __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $STEAM_APP_PATH"
+# Add the following parameters to the custom command in games' property window
+# QT_AUTO_SCREEN_SCALE_FACTOR=0 LD_PRELOAD=/usr/lib64/libfreetype.so.6 __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia %command%
+
+# Torchlight 2 needs an extra env variable
+# SDL_VIDEO_X11_VISUALID= QT_AUTO_SCREEN_SCALE_FACTOR=0 LD_PRELOAD=/usr/lib64/libfreetype.so.6 __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia %command%
