0d0f0c0b8b6b5e038e76831ce3ef740a61278a29
Bruno Parolini <brunoparolini@gmail.com>
2021-05-17 09:09:29 -0300

    Changed kernel removal query and grub update
    Fedora 34 with UEFI uses a different path for grub.cfg

--- scripts/home/fedora_kernel_remove.sh
+++ scripts/home/fedora_kernel_remove.sh
@@ -3,12 +3,12 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Removes old kernel versions and updates grub.cfg
 
-echo "dnf remove -y $(rpm -qa | grep ^kernel | grep -v headers | grep -v $(uname -r) | grep -v srpm-macros)"
-dnf remove -y $(rpm -qa | grep ^kernel | grep -v headers | grep -v $(uname -r) | grep -v srpm-macros)
+echo "dnf remove -y $(dnf repoquery --installonly --latest-limit=-2 -q)"
+dnf remove -y $(dnf repoquery --installonly --latest-limit=-2 -q)
 
 echo
 echo "=============================="
 echo
 
-echo "grub2-mkconfig -o /boot/grub2/grub.cfg"
-grub2-mkconfig -o /boot/grub2/grub.cfg
+echo "grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg"
+grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
