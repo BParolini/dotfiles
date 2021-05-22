65972197adff2ae98343ee164b177d4089d287ae
Bruno Parolini <brunoparolini@gmail.com>
2021-05-22 08:37:52 -0300

    Added a plasma environment variables configuration script

---
+++ apps/plasma_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# author: Bruno Parolini brunoparolini@gmail.com
+# description: Defines environment variables which will be used by Plasma environment.
+#              It must be added to Plasma initialization scripts.
+
+export XDG_CURRENT_DESKTOP=KDE
+export XDG_SESSION_DESKTOP=KDE
