4eff181d2448b3113a15e700d0da72d3d3168ce3
Bruno Parolini <brunoparolini@gmail.com>
2021-07-22 09:19:11 -0300

    Created Docker cleanup script

---
+++ scripts/dev/docker/docker_cleanup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# author: Bruno Parolini brunoparolini@gmail.com
+# description: Clean docker environment
+
+echo "docker rm $(docker ps -qa)"
+docker rm $(docker ps -qa)
+
+echo
+echo "=============================="
+echo
+
+echo "docker volume rm $(docker volume ls -qf dangling=true)"
+docker volume rm $(docker volume ls -qf dangling=true)
+
+echo
+echo "=============================="
+echo
+
+echo "docker network rm $(docker network ls -q)"
+docker network rm $(docker network ls -q)
+
+echo
+echo "=============================="
+echo
+
+echo "docker rmi $(docker images -q)"
+docker rmi $(docker images -q)
