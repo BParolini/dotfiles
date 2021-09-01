00c3bb175dd13848ce9158c97cefb443686ec953
Bruno Parolini <brunoparolini@gmail.com>
2021-09-01 08:57:28 -0300

    Added default namespace to k8s aliases

--- olxbr/kubernetes_envs.sh
+++ olxbr/kubernetes_envs.sh
@@ -7,13 +7,13 @@
 alias k8s-qa='
 kubectl config set-cluster https://api.k8s.qa.vivareal.io --server=https://api.k8s.qa.vivareal.io --insecure-skip-tls-verify &&
 kubectl config set-credentials https://api.k8s.qa.vivareal.io --token="$GH_TOKEN" &&
-kubectl config set-context https://api.k8s.qa.vivareal.io --cluster=https://api.k8s.qa.vivareal.io --user=https://api.k8s.qa.vivareal.io &&
+kubectl config set-context https://api.k8s.qa.vivareal.io --cluster=https://api.k8s.qa.vivareal.io --user=https://api.k8s.qa.vivareal.io --namespace=listings &&
 kubectl config use-context https://api.k8s.qa.vivareal.io &&
 kubectl cluster-info'
 
 alias k8s-prod='
 kubectl config set-cluster https://api.k8s.prod.vivareal.io --server=https://api.k8s.prod.vivareal.io --insecure-skip-tls-verify &&
 kubectl config set-credentials https://api.k8s.prod.vivareal.io --token="$GH_TOKEN" &&
-kubectl config set-context https://api.k8s.prod.vivareal.io --cluster=https://api.k8s.prod.vivareal.io --user=https://api.k8s.prod.vivareal.io &&
+kubectl config set-context https://api.k8s.prod.vivareal.io --cluster=https://api.k8s.prod.vivareal.io --user=https://api.k8s.prod.vivareal.io --namespace=listings &&
 kubectl config use-context https://api.k8s.prod.vivareal.io &&
 kubectl cluster-info'
