5202ad2635d775afb860f1f0774fa5185e9f73a0
Bruno Parolini <brunoparolini@gmail.com>
2021-10-29 14:52:50 -0300

    Added some git configuration

--- scripts/dev/utils/git_config.sh
+++ scripts/dev/utils/git_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Sets Git global configuration (it assumes kwrite and kdiff3 are installed in the system) and installs GitHub CLI
+# description: Sets Git global configuration (it assumes vim and kdiff3 are installed in the system) and installs GitHub CLI
 
 if [ $# -eq 0 ]
 then
@@ -15,10 +15,11 @@ git config --global user.name "$1"
 git config --global user.email "$2"
 git config --global init.defaultBranch master
 git config --global color.ui auto
-git config --global core.editor kwrite
+git config --global core.editor vim
 git config --global merge.tool kdiff3
 git config --global gpg.program gpg2
 git config --global pull.rebase false
+git config --global init.defaultBranch main
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
