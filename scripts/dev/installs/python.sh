4ce01d0bfe20ddd2951820bec4917f82b64d5a6d
Bruno Parolini <brunoparolini@gmail.com>
2022-01-20 07:53:54 -0300

    Added cython to install script

--- scripts/dev/installs/python.sh
+++ scripts/dev/installs/python.sh
@@ -3,8 +3,8 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Installs Kate editor
 
-echo "dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv"
-dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv
+echo "dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv python3-Cython python3-extension-helpers"
+dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv python3-Cython python3-extension-helpers
 
 echo "Preparing pip"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
