266b2b81f6db1470122af9d29722cf2dca8a1271
Bruno Parolini <brunoparolini@gmail.com>
2022-01-24 16:20:13 -0300

    Adjusted AWS configuration

--- olxbr/tokens.sh
+++ olxbr/tokens.sh
@@ -8,6 +8,9 @@ export NPM_TOKEN="***REMOVED***"
 export DOCKER_TOKEN="***REMOVED***"
 export SONAR_TOKEN="***REMOVED***"
 
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_PROFILE=Developers-444914307613
+
 export GH_HIRING_USER="***REMOVED***"
 export GH_HIRING_PASS="***REMOVED***"
 export GH_HIRING_TOKEN="***REMOVED***7"
