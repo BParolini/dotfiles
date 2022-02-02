1f1fd03362b75cf4f3157a229169541b2dfa15fa
Bruno Parolini <brunoparolini@gmail.com>
2022-02-02 17:47:08 -0300

    Updated youtube-dl to the newer yt-dlp

--- scripts/home/utils/youtube_download.sh
+++ scripts/home/utils/youtube_download.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Uses youtube-dl to download best video and audio quality and merges them
+# description: Uses yt-dlp to download best video and audio quality and merges them
 
 if [ $# -eq 0 ]
     echo "Either the video or playlist URL is needed"
     exit -1
 fi
 
-youtube-dl -o '%(title)s.%(ext)s' -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 "$1"
+yt-dlp -o '%(title)s.%(ext)s' -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 "$1"
