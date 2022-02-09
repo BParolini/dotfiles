8110c4a268305ee5e81ba176edd89a3132982e6a
Bruno Parolini <brunoparolini@gmail.com>
2022-02-09 08:49:47 -0300

    Added Python scripts

--- scripts/dev/installs/python.sh
+++ scripts/dev/installs/python.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs Kate editor
+# description: Installs multiple python versions
 
 echo "dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv python3-Cython python3-extension-helpers"
 dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv python3-Cython python3-extension-helpers
 
 echo "Preparing pip"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.6 ./get-pip.py
 python3.7 ./get-pip.py
 python3.8 ./get-pip.py
 python3.9 ./get-pip.py
 python3.10 ./get-pip.py
+
+BASEDIR=$(dirname "$0")
+
+source $BASEDIR/python_package_upgrade.sh python3.6
+source $BASEDIR/python_package_upgrade.sh python3.7
+source $BASEDIR/python_package_upgrade.sh python3.8
+source $BASEDIR/python_package_upgrade.sh python3.9
+source $BASEDIR/python_package_upgrade.sh python3.10
