55a41ba8d72fdf9ddc65a1a7beddedf9e7909ff6
Bruno Parolini <brunoparolini@gmail.com>
2022-02-15 09:24:36 -0300

    Added Python 3.11 to install script

--- scripts/dev/installs/python.sh
+++ scripts/dev/installs/python.sh
@@ -3,8 +3,8 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Installs multiple python versions
 
-echo "dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv python3-Cython python3-extension-helpers"
-dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 pipenv python3-Cython python3-extension-helpers
+echo "dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 python3.11 pipenv python3-Cython python3-extension-helpers"
+dnf install -y python3.6 python3.7 python3.8 python3.9 python3.10 python3.11 pipenv python3-Cython python3-extension-helpers
 
 echo "Preparing pip"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -12,6 +12,7 @@ python3.7 ./get-pip.py
 python3.8 ./get-pip.py
 python3.9 ./get-pip.py
 python3.10 ./get-pip.py
+python3.11 ./get-pip.py
 
 BASEDIR=$(dirname "$0")
 
@@ -20,3 +21,4 @@ source $BASEDIR/python_package_upgrade.sh python3.7
 source $BASEDIR/python_package_upgrade.sh python3.8
 source $BASEDIR/python_package_upgrade.sh python3.9
 source $BASEDIR/python_package_upgrade.sh python3.10
+source $BASEDIR/python_package_upgrade.sh python3.11
