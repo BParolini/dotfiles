29a241881a63e947a62ef321b50d432d75ce2b13
Bruno Parolini <brunoparolini@gmail.com>
2022-03-21 08:23:23 -0300

    Removed all Snap references

--- scripts/home/installs/emulators.sh
+++ scripts/home/installs/emulators.sh
@@ -3,8 +3,6 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Installs various emulators
 
-snap install mgba citra-emu
-
-flatpak install -y --or-update ppsspp RPCS3
+flatpak install -y --or-update org.ppsspp.PPSSPP net.rpcs3.RPCS3 io.mgba.mGBA org.citra_emu.citra
 
 dnf install -y pcsxr pcsx2 dolphin-emu snes9x snes9x-gtk mupen64plus desmume
--- scripts/home/installs/snap_flatpak_installs.sh
+++ scripts/home/installs/snap_flatpak_installs.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env sh
 
 # author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs Snap, Flatpak, flathub and some Snap and Flatpak packages
+# description: Installs Flatpak, flathub and some packages
 
-dnf install -y snapd flatpak
-ln -s /var/lib/snapd/snap /snap
-
-sleep 30
-
-systemctl restart snapd
+dnf install -y flatpak
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Media apps
-snap install spotify
-snap install pocket-casts
-snap install audible-for-linux
-snap install easymp3gain
+flatpak install -y --or-update flathub com.spotify.Client
+flatpak install -y --or-update flathub tech.feliciano.pocket-casts
 
 # Communication apps
-snap install discord
-snap install slack --classic
+flatpak install -y --or-update flathub com.discordapp.Discord
+flatpak install -y --or-update flathub com.slack.Slack
+flatpak install -y --or-update flathub us.zoom.Zoom
