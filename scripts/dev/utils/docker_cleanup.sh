e6ed2afa8824246e34d4236201659ec28646bed9
Bruno Parolini <brunoparolini@gmail.com>
2022-04-01 07:48:00 -0300

    Adjustments to docker scripts

--- scripts/dev/utils/docker_cleanup.sh
+++ scripts/dev/utils/docker_cleanup.sh
@@ -7,22 +7,13 @@ echo "docker rm $(docker ps -qa)"
 docker rm $(docker ps -qa)
 
 echo
-echo "=============================="
-echo
-
 echo "docker volume rm $(docker volume ls -qf dangling=true)"
 docker volume rm $(docker volume ls -qf dangling=true)
 
 echo
-echo "=============================="
-echo
-
 echo "docker network rm $(docker network ls -q)"
 docker network rm $(docker network ls -q)
 
 echo
-echo "=============================="
-echo
-
 echo "docker rmi $(docker images -q)"
 docker rmi $(docker images -q)
--- scripts/dev/utils/docker_prune_all.sh
+++ scripts/dev/utils/docker_prune_all.sh
@@ -3,7 +3,17 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Prunes all Docker images, containers, volumes, etc.
 
+echo "Pruning containers"
 docker container prune --force
+
+echo
+echo "Pruning images"
 docker image prune -a --force
+
+echo
+echo "Pruning volumes"
 docker volume prune --force
+
+echo
+echo "Pruning builder cache"
 docker builder prune --force
