c5df2ff370bfb5cf3598ac4906f9ebf0bb6310f2
Bruno Parolini <brunoparolini@gmail.com>
2022-04-08 08:43:03 -0300

    Adding link instructions to script

--- apps/plasma_env.sh
+++ apps/plasma_env.sh
@@ -4,5 +4,7 @@
 # description: Defines environment variables which will be used by Plasma environment.
 #              It must be added to Plasma initialization scripts.
 
+# ln -s $HOME/.dotfiles/apps/plasma_env.sh $HOME/.config/plasma-workspace/env
+
 export XDG_CURRENT_DESKTOP=KDE
 export XDG_SESSION_DESKTOP=KDE
