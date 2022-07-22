6c45542912bccd4929666e9b9a3e8e5a110f768b
Bruno Parolini <brunoparolini@gmail.com>
2022-07-22 11:42:43 -0300

    Added some dev apps

--- scripts/dev/installs/flatpak_dev_apps.sh
+++ scripts/dev/installs/flatpak_dev_apps.sh
@@ -5,6 +5,8 @@
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+flatpak install -y --or-update com.getpostman.Postman
 flatpak install -y --or-update io.github.shiftey.Desktop # GitHub Desktop
 flatpak install -y --or-update com.github.artemanufrij.regextester
 flatpak install -y --or-update io.dbeaver.DBeaverCommunity
+flatpak install -y --or-update com.github.arshubham.gitignore
