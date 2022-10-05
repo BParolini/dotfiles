0c8d74f30d2884793ea55590f6ae86eddccf7445
Bruno Parolini <bruno.parolini@olxbr.com>
2022-10-05 13:51:56 -0300

    Updated script to work properly in macOS

--- olxbr/scripts/batch_notify_mac.sh
+++ olxbr/scripts/batch_notify_mac.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 
+file_name=temp_notify.json
+
 for f in notify_*
 do
-    echo "Sending file $f"
-    cat "$f" | \
-        xargs -P500 -I {} \
-            curl -X POST "http://listings-api.grupozap.io/v4/listings/actions/notify-to-exporter" \
-                 -H "accept: application/json" -H "Content-Type: application/json" -d "[{}]"
+    gecho "Sending file $f"
+    value=$(gcat "$f" | gsed ':a;N;$!ba;s/\n/,/g;s/,$//g')
+    gecho -n "[$value]" > "$file_name"
+
+    curl "http://listings-api.grupozap.io/v4/listings/actions/notify" \
+        -H "accept: application/json" \
+        -H "Content-Type: application/json" \
+        -d "@$file_name"
+
     sleep 30
 done
+
+rm $file_name
