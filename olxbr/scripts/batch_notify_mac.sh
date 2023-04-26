13fe7a5f37ab519502009cda699be4d75da76cc3
Bruno Parolini <bruno.parolini@olxbr.com>
2023-04-26 11:41:24 -0300

    Improved batch_notify

--- olxbr/scripts/batch_notify_mac.sh
+++ olxbr/scripts/batch_notify_mac.sh
@@ -8,7 +8,10 @@ then
 fi
 
 mkdir -p ./split_files
-gsplit -dl 5000 -a 3 "$1" split_files/notify_
+temp_file="temp_uniq_file"
+gcat "$1" | uniq > "$temp_file"
+gsplit -dl 5000 -a 3 "$temp_file" split_files/notify_
+rm -f "$temp_file"
 
 cd ./split_files
 file_name=temp_notify.json
@@ -16,7 +19,7 @@ file_name=temp_notify.json
 for f in notify_*
 do
     gecho "Sending file $f"
-    value=$(gcat "$f" | gsed ':a;N;$!ba;s/\n/,/g;s/,$//g')
+    value=$(gcat "$f" | gsed ':a;N;$!ba;s/\n/,/g;s/,$//g' | tr -d "'")
     gecho -n "[$value]" > "$file_name"
 
     curl "http://listings-api.grupozap.io/v4/listings/actions/notify" \
