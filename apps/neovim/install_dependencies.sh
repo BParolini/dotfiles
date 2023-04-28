b9c0caa207f5072ebccafaeb3109ce921496611d
Bruno Parolini <brunoparolini@gmail.com>
2023-04-28 08:29:54 -0300

    LSP installation moved to its own file

--- apps/neovim/install_dependencies.sh
+++ apps/neovim/install_dependencies.sh
@@ -6,8 +6,6 @@
 BASEDIR="$(dirname "$(realpath "$0")")"
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
-git clone --depth 1 git@github.com:eclipse/eclipse.jdt.ls.git "$HOME/.jdtls"
-go install github.com/go-delve/delve/cmd/dlv@latest
 
 rm -rf "$HOME/.config/nvim"
 ln -sfF "$BASEDIR/config_nvim" "$HOME/.config/nvim"
