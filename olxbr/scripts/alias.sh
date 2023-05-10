1fd0ea2230f2474c34ee99b2a940e5c8698046aa
Bruno Parolini <bruno.parolini@olxbr.com>
2023-05-10 08:56:41 -0300

    Added an alias script for flatpak apps

--- olxbr/scripts/alias.sh
+++ olxbr/scripts/alias.sh
@@ -3,8 +3,6 @@
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Creates alias for flatpak apps
 
-alias code='flatpak run com.visualstudio.code'
-alias nvim='flatpak run io.neovim.nvim'
 alias firefox='flatpak run org.mozilla.firefox'
 alias simplenote='flatpak run com.simplenote.Simplenote'
 alias postman='flatpak run com.getpostman.Postman'
