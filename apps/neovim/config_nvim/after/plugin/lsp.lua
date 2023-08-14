67e2511164db5536896042b699d826fd49a4f7e2
Bruno Parolini <brunoparolini@gmail.com>
2023-08-13 21:54:19 -0300

    Various languages and configurations adjusted

--- apps/neovim/config_nvim/after/plugin/lsp.lua
+++ apps/neovim/config_nvim/after/plugin/lsp.lua
@@ -10,9 +10,13 @@ lsp.ensure_installed({
     "emmet_ls",
     "gopls",
     "gradle_ls",
+    "groovyls",
+    "hls",
     "jdtls",
+    "kotlin_language_server",
     "lua_ls",
     "marksman",
+    "ocamllsp",
     "pylsp",
     "rust_analyzer",
     "tsserver",
@@ -20,16 +24,16 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({select=true}),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-space>'] = cmp.mapping.complete(),
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr , remap = false}
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
--- apps/neovim/config_nvim/after/plugin/treesitter.lua
+++ apps/neovim/config_nvim/after/plugin/treesitter.lua
@@ -1,51 +1,52 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {
-    "c",
-    "cmake",
-    "cpp",
-    "css",
-    "diff",
-    "gitignore",
-    "go",
-    "gomod",
-    "gosum",
-    "html",
-    "java",
-    "javascript",
-    "json",
-    "kotlin",
-    "make",
-    "markdown",
-    "markdown_inline",
-    "lua",
-    "proto",
-    "python",
-    "query",
-    "rust",
-    "sql",
-    "toml",
-    "typescript",
-    "vim",
-    "vimdoc",
-    "vue",
-    "yaml"
-  },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = {
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "diff",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "kotlin",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "ocaml",
+        "proto",
+        "python",
+        "query",
+        "rust",
+        "sql",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "vue",
+        "yaml"
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
