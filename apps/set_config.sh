54d92a5353dac02f1258af9a324443420de42aff
Bruno Parolini <bruno.parolini@olxbr.com>
2023-08-14 09:23:29 -0300

    Added .ideavimrc link creation

--- apps/set_config.sh
+++ apps/set_config.sh
@@ -14,7 +14,9 @@ ln -sf "$BASEDIR/zshrc" "$HOME/.zshrc"
 . "$HOME/.dotfiles/apps/xdg_defaults.sh"
 
 rm -f "$HOME/.vimrc"
+rm -f "$HOME/.ideavimrc"
 ln -sf "$BASEDIR/vimrc" "$HOME/.vimrc"
+ln -sf "$BASEDIR/vimrc" "$HOME/.ideavimrc"
 
 git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 rm -f "$HOME/.tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
