12de5398bb23f09aa025d6c7bd1684164e7577e1
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-05 15:38:49 -0300

    Removed packer config

--- apps/neovim/install_dependencies.sh
+++ apps/neovim/install_dependencies.sh
@@ -5,7 +5,5 @@
 
 BASEDIR="$(dirname "$(realpath "$0")")"
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
-
 rm -rf "$HOME/.config/nvim"
 ln -sfF "$BASEDIR/config_nvim" "$HOME/.config/nvim"
