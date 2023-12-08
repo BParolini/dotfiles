9bc216c39ee14429b9b36a303e0ee263a5cb96e8
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-08 15:53:18 -0300

    Excluded `node_modules` from recursive grep

--- apps/alias.sh
+++ apps/alias.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-alias grep='grep -Hn --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.vscode,.fleet,.venv}'
-alias egrep='grep -HnE --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.vscode,.fleet,.venv}'
-alias fgrep='grep -HnF --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.vscode,.fleet,.venv}'
+alias grep='grep -Hn --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.vscode,.fleet,.venv,node_modules}'
+alias egrep='grep -HnE --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.vscode,.fleet,.venv,node_modules}'
+alias fgrep='grep -HnF --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,.vscode,.fleet,.venv,node_modules}'
