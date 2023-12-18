a9c5f4fddf121e3b12e79040b665db69250b8fcf
Bruno Parolini <bruno.parolini@olxbr.com>
2023-12-18 08:37:08 -0300

    Removed neovim config from this repo

--- neovim/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# author: Bruno Parolini brunoparolini@gmail.com
-# description: Installs NeoVIM configuration dependencies
-
-BASEDIR="$(dirname "$(realpath "$0")")"
-
-rm -rf "$HOME/.config/nvim"
-ln -sfF "$BASEDIR/config_nvim" "$HOME/.config/nvim"
