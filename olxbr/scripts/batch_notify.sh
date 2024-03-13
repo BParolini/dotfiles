7475b921bd019a8e14b038e9c487b74fe45152c9
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-13 16:57:18 -0300

    Improved notification script

--- olxbr/scripts/batch_notify.sh
+++ olxbr/scripts/batch_notify.sh
@@ -1,19 +1,46 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if [ $# -eq 0 ]
+if [ $# -lt 4 ]
 then
     echo "No arguments supplied."
-    echo "You must inform the csv file to be notified"
+    echo "Usage: $0 <environment> <action> <file> <line_nr> [wait seconds]"
     exit 1
 fi
 
+declare -A environments=(
+    ["prod"]="http://listings-api.grupozap.io/v4/listings/actions"
+    ["qa"]="http://qa-listings-api.grupozap.io/v4/listings/actions"
+)
+
+declare -A actions=(
+    ["crude"]="notify-to-crude"
+    ["unit"]="notify-to-unit"
+    ["exporter"]="notify-to-exporter"
+    ["notify"]="notify"
+    ["dedup"]="notify-dedup-image"
+)
+
+environment="$1"
+action="$2"
+file="$3"
+line_nr="$4"
+endpoint="${environments[$environment]}/${actions[$action]}"
+wait_time=${5:-20}
+
+echo "Endpoint: $endpoint"
+echo "Wait time: $wait_time"
+echo "File: $file - $(wc -l < "$file") lines"
+echo "Lines per iteration: $line_nr"
+read -p "Deseja continuar? (Y/n): " -n 1 -r confirm && [[ $confirm =~ ^[yYsS]$ ]] || exit 1
+echo
+
 WORK_DIR=$(mktemp -d)
-split -dl 10000 -a 3 "$1" "$WORK_DIR/notify_"
+split -dl "$line_nr" -a 3 "$file" "$WORK_DIR/notify_"
 
 (
     cd "$WORK_DIR" || exit 1
     echo "Entered $WORK_DIR"
-    echo "Files: $(fd --maxdepth 1 --type f . | wc -l)"
+    echo "Files: $(find . -maxdepth 1 -type f | wc -l)"
 
     for f in notify_*
     do
@@ -23,12 +50,12 @@ split -dl 10000 -a 3 "$1" "$WORK_DIR/notify_"
 
         curl_response=$(
             curl -s -w "%{http_code}" \
-                -X POST "http://listings-api.grupozap.io/v4/listings/actions/notify-to-crude" \
+                -X POST "$endpoint" \
                 -H "accept: application/json" -H "Content-Type: application/json" -d "[$ids]"
         )
         echo "Request response for \"$f\": $curl_response"
 
-        sleep 20
+        sleep "$wait_time"
     done
 )
 
