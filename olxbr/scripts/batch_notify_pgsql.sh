fc33ddbfa9f69d03281aba9bb658ba9cbeefb4f6
Bruno Parolini <bruno.parolini@olxbr.com>
2024-03-13 12:17:47 -0300

    Improved notification scripts

--- olxbr/scripts/batch_notify_pgsql.sh
+++ olxbr/scripts/batch_notify_pgsql.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+: "${PGPASSWORD:?"No value supplied for PGPASSWORD"}"
+
 if [ $# -eq 0 ]
 then
     echo "No arguments supplied."
@@ -12,14 +14,17 @@ split -dl 10000 -a 3 "$1" "$WORK_DIR/notify_"
 
 (
     cd "$WORK_DIR" || exit 1
-    echo "Entered $WORK_DIR"
-    echo "Files: $(fd --maxdepth 1 --type f . | wc -l)"
 
     for f in notify_*
     do
         echo "Sending file $f"
 
         ids=$(sed ':a;N;$!ba;s/\n/,/g;s/,$//g' < "$f")
+
+        # update da base pela linha de comando mesmo
+        psql -h db.listings.listings-management.private.prod.grupozap.io -p 5432 \
+            -d listings -U master \
+            -c "update listing set scores = '[]'::jsonb where id in ($ids)"
 
         curl_response=$(
             curl -s -w "%{http_code}" \
