1543eaad029a7e4c1c190cd988cc520fb45b90c9
Bruno Parolini <bruno.parolini@olxbr.com>
2024-05-04 19:00:12 -0300

    Added alacritty config for mac and adjusted setup script

--- alacritty/set_config.sh
+++ alacritty/set_config.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Removes the original configuration files and creates a symlink to the repository ones
 
 BASEDIR="$(dirname "$(realpath "$0")")"
 
+if [[ "$OSTYPE" == "linux"* ]]; then
+    config_file="$BASEDIR/alacritty.toml"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    config_file="$BASEDIR/alacritty_mac.toml"
+fi
+
 rm -f "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
 mkdir -p "$XDG_CONFIG_HOME/alacritty"
-ln -sf "$BASEDIR/alacritty.toml" "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
+ln -sf "$config_file" "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
