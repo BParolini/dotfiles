783b0620944f53ac592705dbe0f1089ebab4025e
Bruno Parolini <bruno.parolini@olxbr.com>
2024-05-05 11:48:37 -0300

    Updated config script to identify OS and choose the correct configuration

--- hosts/set_config.sh
+++ hosts/set_config.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Removes the original configuration files and creates a symlink to the repository ones
 
 BASEDIR="$(dirname "$(realpath "$0")")"
 
+if [[ "$OSTYPE" == "linux"* ]]; then
+    config_file="$BASEDIR/hosts"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    config_file="$BASEDIR/hosts_olx"
+fi
+
 sudo rm -f /etc/hosts
-sudo ln -sf "$BASEDIR/hosts" /etc/hosts
+sudo ln -sf "$config_file" /etc/hosts
