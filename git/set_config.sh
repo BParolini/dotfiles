cc058a611f69bd802b3624ff2f1070c306bd5a2d
Bruno Parolini <bruno.parolini@olxbr.com>
2024-05-05 11:48:26 -0300

    Updated config script to identify OS and choose the correct configuration

--- git/set_config.sh
+++ git/set_config.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 # author: Bruno Parolini brunoparolini@gmail.com
 # description: Removes the original configuration file and creates a symlink to the repository one
 
 BASEDIR="$(dirname "$(realpath "$0")")"
 
+if [[ "$OSTYPE" == "linux"* ]]; then
+    config_file="$BASEDIR/gitconfig"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    config_file="$BASEDIR/gitconfig_olxbr"
+fi
+
 rm -f "$HOME/.gitconfig"
-ln -sf "$BASEDIR/gitconfig" "$HOME/.gitconfig"
+ln -sf "$config_file" "$HOME/.gitconfig"
