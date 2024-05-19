54e8824c52ee1658383f2151cb8e529e1da44d7f
Bruno Parolini <bruno.parolini@olxbr.com>
2024-05-19 14:36:47 -0300

    Updated zsh config

--- zsh/set_config.sh
+++ zsh/set_config.sh
@@ -8,8 +8,9 @@ BASEDIR="$(dirname "$(realpath "$0")")"
 rm -f "$HOME/.zshenv"
 ln -sf "$BASEDIR/zshenv" "$HOME/.zshenv"
 
-mkdir -p "$HOME/.config/zsh/external"
 rm -f "$HOME/.zshrc"
 ln -sf "$BASEDIR/zshrc" "$HOME/.config/zsh/.zshrc"
 ln -sf "$BASEDIR/aliases" "$HOME/.config/zsh/aliases"
+ln -sf "$BASEDIR/dev_env" "$HOME/.config/zsh/dev_env"
+ln -sf "$BASEDIR/xdg_defaults" "$HOME/.config/zsh/xdg_defaults"
 
