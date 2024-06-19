45fcca36042b1b89687a51d4aaa93879531e684e
Bruno Parolini <bruno.parolini@olxbr.com>
2024-06-18 21:22:29 -0300

    More stow changes (alacritty and neovim)

--- config_packages.sh
+++ config_packages.sh
@@ -6,17 +6,19 @@
 BASEDIR="$(dirname "$(realpath "$0")")"
 
 packages=(
-    zsh
-    vim
-    tmux
-    ssh
-    aws
+    "zsh"
+    "vim"
+    "tmux"
+    "ssh"
+    "aws"
     )
 
 # Run GNU stow in all repo packages
 for p in "${packages[@]}"; do
     stow -d "$BASEDIR" -t "$HOME" --dotfiles "$p"
 done
+
+source ./neovim/config_nvim.sh
 
 # Installs brew only if in MacOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -26,5 +28,19 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     rm -f ~/Brewfile
     stow -d "$BASEDIR" brew
-    brew bundle install
+    brew bundle install --file ~/Brewfile
+
+    stow -d "$BASEDIR" -t "$HOME" --dotfiles alacritty-mac
+else
+    stow -d "$BASEDIR" -t "$HOME" --dotfiles alacritty
+fi
+
+# Installs or updates tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+else
+    (
+        cd "$HOME/.tmux/plugins/tpm" || exit
+        git pull
+    )
 fi
